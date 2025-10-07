a40643fada0f29dd3282355a4494caf2cf208799
Gonzalo Aellen <gonzaloaellen@gmail.com>
2025-10-06 22:54:20 -0300

    Agrega Repositorios

---
+++ src/repositories/citaRepository.mjs
@@ -0,0 +1,70 @@
+import Cita from '../models/cita.mjs';
+
+const citaRepository = {
+
+    async crearCita(data) {
+        try { 
+            const cita = new Cita(data);
+            const citaGuardada = await cita.save();
+            return citaGuardada;
+        }catch (error) {
+            console.error("Error al crear cita:", error);
+        }
+    },
+
+    async obtenerPorId(id) {
+        try{
+            return await Cita.findById(id)
+            .populate('Propietario', '-Password');
+        }catch (error) {
+            console.error("Error al obtener cita por ID:", error);
+        }
+    },
+
+    async obtenerPorPropietario(propietarioId) {
+        try{
+            return await Cita.find({ Propietario: propietarioId })
+            .populate('Propietario', '-Password');
+        }catch (error) {
+            console.error("Error al obtener cita por propietario:", error);
+        }
+    },
+    
+    async obtenerPorFechas(data) { 
+        try{
+            const { fechaInicio, fechaFin } = data;
+            return await Cita.find({
+                Fecha: { $gte: fechaInicio, $lte: fechaFin }
+            }).populate('Propietario', '-Password');
+        }catch (error) {
+            console.error("Error al obtener cita por fechas:", error);
+        }
+    },
+
+    async obtenerPorMascota(mascotaId) {
+        try{
+            return await Cita.find({ Mascota: mascotaId }).populate('Propietario', '-Password');
+        }catch (error) {
+            console.error("Error al obtener cita por mascota:", error);
+        }
+    },
+
+    async editarCita(data) {
+        try{
+            const { id, cita } = data;
+            return await Cita.findByIdAndUpdate(id, cita, { new: true });
+        }catch (error) {
+            console.error("Error al actualizar cita:", error);
+        }
+    },
+
+    async eliminarCita(id) {
+        try{
+            return await Cita.findByIdAndDelete(id);
+        }catch (error) {
+            console.error("Error al eliminar cita:", error);
+        }
+    }
+};
+
+export default citaRepository;
